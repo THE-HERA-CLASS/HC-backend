dd3df39430aacba3842d2ab186df400e4bc95bb0
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-05-22 15:43:20 +0900

    [Init] app.js init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const port = process.env.HOST_PORT;
+
+const cookieParser = require('cookie-parser');
+const router = require('router');
+var cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api', router);
+
+app.listen(port, () => {
+  console.log(`Server open on port ${port}`);
+});
