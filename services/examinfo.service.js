b4f7e79f52ab7bc2d877826ccfeb89550c7b9317
kimchaeminthegreat <125367831+kimchaeminthegreat@users.noreply.github.com>
2023-05-24 10:20:23 +0900

    [Feat] api/examinfo 자격증 추가

--- services/examinfo.service.js
+++ services/examinfo.service.js
@@ -3,6 +3,8 @@ const ExaminfoRepository = require('../repositories/examinfo.repository.js');
 class ExaminfoService {
   examinfoRepository = new ExaminfoRepository();
 
+//[전공]=================================================================    
+
   addMajor = async (name) => {
     try {
       return await this.examinfoRepository.addMajor(name);
@@ -46,7 +48,54 @@ class ExaminfoService {
     }
   }
 
-  };
+
+//[자격증]=================================================================
+addCertificate = async (major_id, name, division) => {
+  try {
+    return await this.examinfoRepository.addCertificate(major_id, name, division);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// getCertificate = async () => {
+//   try {
+//     const majorData = await this.examinfoRepository.getCertificate();
+//   //   const majorPrint = majorData.map((item) => {
+//   //     return { 
+//   //         id: item.major_id,
+//   //         name: item.name,
+//   //     }
+//   //   })
+//   //   const majorOne = {
+//   //     id: majorData.major_id,
+//   //     name: majorData.name,
+//   //   } 
+//     return majorData;
+//   } catch (err) {
+//       console.err(err);
+//   }
+// }
+
+// updateCertificate = async (name, major_id) => {
+//   try {
+//     return await this.examinfoRepository.updateCertificate(name, major_id);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+// dropCertificate = async (major_id) => {
+//   try {
+//     return await this.examinfoRepository.dropCertificate(major_id);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+
+
+};
 
 
 
