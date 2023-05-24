4ce0e8b0dca8bdd9c05813710bc859088468a71e
junseok1925 <rkdwnstjr1925@naver.com>
2023-05-24 22:34:07 +0900

    [fix] redis 저장시 생기는 typeErr 수정
    redis에 key: user_id, value: refreshToke의 값을 넣으려 시도
    typeError 발생
    redis의 key에는 String type만 저장 가능
    user_id는 number type 이므로 ```redis.repository.js```에서 setData 함수에 key,value값을 넣을때 String(key)로 감싸서 해결

--- controllers/examinfo.controller.js
+++ controllers/examinfo.controller.js
@@ -3,6 +3,8 @@ const ExaminfoService = require('../services/examinfo.service.js');
 class ExaminfoController {
   examinfoService = new ExaminfoService();
 
+//[전공]=================================================================  
+
   addMajor = async (req, res) => {
     try {
         // const { user_id } = res.locals.user;
@@ -69,6 +71,144 @@ class ExaminfoController {
         return res.status(400).json({ errMsg: '전공 삭제 실패'})
     }    
   };
+
+//[자격증]=================================================================
+
+  addCertificate = async (req, res) => {
+    try {
+        // const { user_id } = res.locals.user;
+        const { major_id, name, division } = req.body;
+
+              
+        await this.examinfoService.addCertificate(major_id, name, division);
+        return res.status(200).json({ msg: '자격증 추가 완료' });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({ errMsg: '자격증 추가 실패'})
+    }
+  };
+
+  getCertificate = async (req, res) => {
+    try {
+        // const { user_id } = res.locals.user;
+      
+
+        const certificateData = await this.examinfoService.getCertificate();
+        return res.status(200).json({ certificateData });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({ errMsg: '자격증 보기 실패'})
+    }
+  };
+
+  updateCertificate = async (req, res) => {
+    try {
+        // const { user_id } = res.locals.user;
+        const { certificate_id } = req.params;
+        const { name, division } = req.body;
+              
+        const updateResult = await this.examinfoService.updateCertificate(certificate_id, name, division);
+        
+        if (updateResult) {
+          return res.status(200).json({ msg: '자격증 수정 완료' });
+        } else {
+          return res.status(419).json({ errMsg: '자격증 수정 실패'});
+        }
+
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({ errMsg: '자격증 수정 실패'})
+    }
+  };
+
+  dropCertificate = async (req, res) => {
+    try {
+        // const { user_id } = res.locals.user;
+        const { certificate_id } = req.params;
+              
+        const dropResult = await this.examinfoService.dropCertificate(certificate_id);
+        
+        if (dropResult) {
+          return res.status(200).json({ msg: '자격증 삭제 완료' });
+        } else {
+          return res.status(419).json({ errMsg: '자격증 삭제 실패'});
+        }        
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({ errMsg: '자격증 삭제 실패'})
+    }    
+  }; 
+
+//[과목]=================================================================
+
+addSubject = async (req, res) => {
+  try {
+      // const { user_id } = res.locals.user;
+      const { certificate_id, name } = req.body;
+
+            
+      await this.examinfoService.addSubject(certificate_id, name);
+      return res.status(200).json({ msg: '과목 추가 완료' });
+
+  } catch (error) {
+      console.error(error);
+      return res.status(400).json({ errMsg: '과목 추가 실패'})
+  }
+};
+
+getSubject = async (req, res) => {
+  try {
+      // const { user_id } = res.locals.user;
+    
+
+      const subjectData = await this.examinfoService.getSubject();
+      return res.status(200).json({ subjectData });
+
+  } catch (error) {
+      console.error(error);
+      return res.status(400).json({ errMsg: '과목 보기 실패'})
+  }
+};
+
+updateSubject = async (req, res) => {
+  try {
+      // const { user_id } = res.locals.user;
+      const { subject_id } = req.params;
+      const { name } = req.body;
+            
+      const updateResult = await this.examinfoService.updateSubject(subject_id, name);
+      
+      if (updateResult) {
+        return res.status(200).json({ msg: '과목 수정 완료' });
+      } else {
+        return res.status(419).json({ errMsg: '과목 수정 실패'});
+      }
+
+  } catch (error) {
+      console.error(error);
+      return res.status(400).json({ errMsg: '과목 수정 실패'})
+  }
+};
+
+dropSubject = async (req, res) => {
+  try {
+      // const { user_id } = res.locals.user;
+      const { subject_id } = req.params;
+            
+      const dropResult = await this.examinfoService.dropSubject(subject_id);
+      
+      if (dropResult) {
+        return res.status(200).json({ msg: '과목 삭제 완료' });
+      } else {
+        return res.status(419).json({ errMsg: '과목 삭제 실패'});
+      }        
+  } catch (error) {
+      console.error(error);
+      return res.status(400).json({ errMsg: '과목 삭제 실패'})
+  }    
+};
   
 };
 
