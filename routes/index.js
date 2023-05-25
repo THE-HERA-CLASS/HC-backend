fc50f76e79bf3e26e07868a40657978f6f3e8962
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-05-25 20:19:24 +0900

    conflict commit

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userRouter = require('./user.route.js');
+const examinfoRouter = require('./examinfo.route.js');
+const examRouter = require('./exam.route.js');
 const loginRouter = require('./login.route.js');
 
-router.use('/', [userRouter]);
-router.use('/', [loginRouter]);
+router.use('/', [userRouter, loginRouter, examinfoRouter, examRouter]);
 
 module.exports = router;
