2547ebb05cb661c6a276110cfe87395ed6be2ec9
junseok1925 <rkdwnstjr1925@naver.com>
2023-05-27 13:52:23 +0900

    [refactor] cookie accToken에 Bearer 타입 명시추가

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -32,7 +32,7 @@ class LoginController {
       major_id: user.major_id,
     };
     // cookie에 저장
-    res.cookie('accessToken', `${accessToken}`);
+    res.cookie('accessToken', `Bearer ${accessToken}`);
     return res
       .status(200)
       .json({ msg: '로그인 성공(refreshToken 확인용으로 표시 보안상 나중에 지우도록)', accessToken, refreshToken, userData });
