5f04673a0b3d01f661d7972d1881818f39d4ff68
junseok1925 <rkdwnstjr1925@naver.com>
2023-05-31 22:16:03 +0900

    [refactor] multer-S3.js 화살표함수로 리팩토링

--- middlewares/multer-S3.js
+++ middlewares/multer-S3.js
@@ -15,6 +15,23 @@ const s3 = new AWS.S3({
 // 허용할 파일 확장자
 const allowedExtensions = ['.png', '.jpg', '.jpeg', '.bmp', '.gif'];
 
+// 현재 날짜와 시간
+const getToDate = () => {
+  const now = new Date();
+  return `${now.getFullYear()}-${
+    now.getMonth() + 1
+  }-${now.getDate()}-${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}`;
+};
+
+// 랜덤한 4자리 숫자
+const getRandomNum = () => {
+  let result = '';
+  for (let i = 0; i < 4; i++) {
+    result += String(Math.floor(Math.random() * 10));
+  }
+  return result;
+};
+
 const upload_image = multer({
   storage: multerS3({
     s3: s3, // s3객체 S3와 상호작용
@@ -23,14 +40,8 @@ const upload_image = multer({
 
     // S3에 저장될 때 사용할 파일명을 설정
     key: (req, file, callback) => {
-      const today = new Date(); // 현재시간 정보
-      const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}-${today.getHours()}-${today.getMinutes()}-${today.getSeconds()}`;
-
-      // 램던 숫자 저장
-      let randomNumber = ''; // 빈배열로 초기화
-      for (let i = 0; i < 4; i++) {
-        randomNumber += String(Math.floor(Math.random() * 10));
-      }
+      const today = getToDate();
+      const randomNum = getRandomNum();
       // from 데이터로 받은파일의 확장자를 추출
       const extension = path.extname(file.originalname).toLowerCase();
 
@@ -39,15 +50,13 @@ const upload_image = multer({
         return callback(new Error('확장자 에러')); //412
       }
       // 파일이 업로드된 시간(date), 랜덤한 네 자리 숫자(randomNumber)를 결함하려 고유한이름을 가진 파일의 url를 생성
-      const img_url = `https://theheraclass.s3.ap-northeast-2.amazonaws.com/img/${date}_${randomNumber}${extension}`;
+      const img_url = `https://theheraclass.s3.ap-northeast-2.amazonaws.com/img/${today}_${randomNum}${extension}`;
 
       // 생성된 img_url을 req.img_url에 배열 형태로 담음
-      if (!req.img_url) {
-        req.img_url = [];
-      }
+      req.img_url = req.img_url || [];
       // img_url 값을 req.img_url 배열에 추가(push)
       req.img_url.push(img_url);
-      callback(null, `img/${date}_${randomNumber}${extension}`);
+      callback(null, `img/${today}_${randomNum}${extension}`);
     },
     // AWS S3에서 액세스 제어 목록 (ACL)을 설정 이 경우 "public-read"는 파일이 공개적으로 읽을 수 있음을 의미 즉, 인터넷 상의 모든 사람이 해당 파일을 읽을 수 있다.
     acl: 'public-read',
