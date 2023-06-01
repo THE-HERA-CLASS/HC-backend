b3f351f37a6beccdb87f1e707c10649e0ed6a1ef
junseok1925 <rkdwnstjr1925@naver.com>
2023-06-01 19:49:23 +0900

    [init] 검색api

---
+++ services/search.service.js
@@ -0,0 +1,32 @@
+const SearchRepository = require('../repositories/search.repository');
+
+class SearchService {
+  searchRepository = new SearchRepository();
+
+  searchExams = async (keyword) => {
+    try {
+      const results = await this.searchRepository.searchExams(keyword);
+      
+      if (!results) {
+        return [];
+      }
+
+      return results.map((result) => {
+        if (result.certificate_name.includes(keyword)) {
+          return {
+            certificate_name: result.certificate_name,
+            subject_name: result.subject_name,
+          };
+        } else {
+          return {
+            subject_name: result.subject_name,
+          };
+        }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+}
+
+module.exports = SearchService;
