ce4d3ca4a85c613ef681ab4ce5db87bbcbc25c52
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-06-07 00:59:57 +0900

    [Feat] 회원 답안 제출 기능 구현 완료

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,22 @@ const router = express.Router();
 
 const userRouter = require('./user.route.js');
 const examinfoRouter = require('./examinfo.route.js');
-const examRouter = require('./question.route.js');
+const questionRouter = require('./question.route.js');
 const loginRouter = require('./login.route.js');
 const imageRouter = require('./image.router.js');
 const searchRouter = require('./search.router.js');
+const xnoteRouter = require('./xnote.route.js');
+const bookmarkRouter = require('./bookmark.route.js');
 
-router.use('/', [userRouter, loginRouter, examinfoRouter, examRouter,imageRouter,searchRouter]);
-
+router.use('/', [
+  userRouter,
+  loginRouter,
+  examinfoRouter,
+  questionRouter,
+  imageRouter,
+  searchRouter,
+  xnoteRouter,
+  bookmarkRouter,
+]);
 
 module.exports = router;
