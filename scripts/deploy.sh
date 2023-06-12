3f9f63443af0e2fb30a07f565cf327f48f49b0f2
kimchaeminthegreat <125367831+kimchaeminthegreat@users.noreply.github.com>
2023-06-12 20:57:01 +0900

    [Fix] CD를 위한 해당 파일들 내용 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-sudo \cp -rf /home/ubuntu/temp /home/ubuntu/HC-backend
-sudo cp /home/ubuntu/.env /home/ubuntu/HC-backend/
-cd /home/ubuntu/HC-backend 
-sudo npm install
-pm2 start /home/ubuntu/HC-backend/app.js
 
-if [ -d /home/ubuntu/temp ]; then
-    sudo rm -rf /home/ubuntu/temp
-fi
+pm2 restart 0
+echo "BE배포"
