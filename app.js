f1c120c9c21d6050d93e8f8a04ff6108683bcde5
kang jun seok <rkdwnstjr1925@naver.com>
2023-06-14 01:47:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler.js');
 var cors = require('cors');
 
-// 로컬과 프론트엔드도메인 둘다 허용함
+// 로컬과 프론트엔드도메인 둘다 허용함 왜냐면 프로트엔드가 로컬에서 테스트해야해서
 app.use(
   cors({
     origin: ['https://hera-class.com', 'http://localhost:3000'],
