1b70c601855bdf5bba5c061fdc677a63fa66d090
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-06-24 22:03:01 +0900

    [Feat] Controller Unit Test : Majors

---
+++ __tests__/unit/majors.controller.unit.spec.js
@@ -0,0 +1,111 @@
+const ExaminfoController = require('../../controllers/examinfo.controller.js');
+
+let mockExaminfoService = {
+    addMajor: jest.fn(),
+    getMajors: jest.fn(),
+    getOneMajor: jest.fn(),
+    updateMajor: jest.fn(),
+    dropMajor: jest.fn(),
+}
+
+let mockRequest = {
+    body: jest.fn(),
+}
+
+let mockResponse = {
+    status: jest.fn(),
+    json: jest.fn(),
+}
+
+let examinfoController = new ExaminfoController();
+
+examinfoController.examinfoService = mockExaminfoService;
+
+describe('Examinfo Controller Unit Test', () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+
+        mockResponse.status = jest.fn(() => {
+            return mockResponse;
+        });
+    });
+
+    test('Major addMajor Unit Test : Success', async () => {
+        const addMajorRequestBody = {
+            name: 'request name'
+        }
+
+        mockRequest.body = addMajorRequestBody;
+
+        const addMajorReturnValue = { msg: '전공 등록 완료' }
+
+        mockExaminfoService.addMajor = jest.fn(() => {
+            return addMajorReturnValue
+        })
+
+        await examinfoController.addMajor(mockRequest, mockResponse);
+
+        // 1. Request body 데이터가 저상적으로 addMajor에 전달되는가?
+        expect(mockExaminfoService.addMajor).toHaveBeenCalledTimes(1);
+        expect(mockExaminfoService.addMajor).toHaveBeenCalledWith(addMajorRequestBody.name);
+
+        // 2-1. mockResponse.json 결과값이 우리가 의도한 대로 잘 나오냐?
+        expect(mockResponse.json).toHaveBeenCalledTimes(1);
+        expect(mockResponse.json).toHaveBeenCalledWith({ msg: '전공 등록 완료'});
+
+        // 3. mockResponse.status 200이 맞냐?
+        expect(mockResponse.status).toHaveBeenCalledTimes(1);
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+    })
+
+    test('Major addMajor Unit Test: name null', async () => {
+        mockRequest.body = {};
+
+        await examinfoController.addMajor(mockRequest, mockResponse);
+
+        // 1. 요청 상태값이 400이냐?
+        expect(mockResponse.status).toHaveBeenCalledTimes(1);
+        expect(mockResponse.status).toHaveBeenCalledWith(411);
+
+        // 2. 에러문구가 우리가 예상하는게 맞냐?
+        expect(mockResponse.json).toHaveBeenCalledWith({ errMsg: '값 없음: name' })
+    })
+
+    test('Major getMajors Unit Test', async () => {
+        const getMajorsReturnValues = [
+            {
+                "major_id": 1,
+                "name": "컴퓨터공학"
+            },
+            {
+                "major_id": 2,
+                "name": "간호학"
+            },
+        ]
+    
+        mockExaminfoService.getMajors = jest.fn(() => {
+            return getMajorsReturnValues
+        })
+
+        await examinfoController.getMajors(mockRequest, mockResponse);
+
+        // 1. 서비스쪽에 getMajors 메서드 요청 1번만 하느냐?
+        expect(mockExaminfoService.getMajors).toHaveBeenCalledTimes(1);
+
+        // 2. 최종 상태값이 200이냐?
+        expect(mockResponse.status).toHaveBeenCalledTimes(1);
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+
+        // 3. 너가 예상하는 결과대로 나오냐?
+        expect(mockResponse.json).toHaveBeenCalledWith({ data: getMajorsReturnValues });
+    })
+
+    // test('Major addMajor Unit Test', async () => {
+
+    // })
+
+    // test('Major addMajor Unit Test', async () => {
+
+    // })
+
+})
